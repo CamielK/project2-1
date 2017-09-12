fce28e725a49c678c8494877badb526fbb9cc8e3
Camiel <camielk@live.nl>
2017-09-12 12:42:10 +0200

    Initial commit

---
+++ nothanks/src/Main.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("gui/interface.fxml"));
+        primaryStage.setTitle("No thanks!");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+}
