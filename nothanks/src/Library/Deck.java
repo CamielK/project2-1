dc33a1944684c6c46fa4343b25e30f4bcd8154db
DanielKaestner@outlook.de <DanielKaestner@outlook.de>
2017-09-14 12:56:02 +0200

    few Comments

--- nothanks/src/Library/Deck.java
+++ nothanks/src/Library/Deck.java
@@ -6,20 +6,28 @@ import java.util.Collections;
 
 public class Deck extends ArrayList<Card> {
 
+    /**
+     * Construct the Deck and initialise the Cards from 3-35
+     */
     public Deck() {
         for(int i = 3; i <= 35; i++) {
         	this.add(new Card(i));
         }
     }
 
+    /**
+     * shuffels the cards in the deck
+     */
     private void shuffleCards(){
         Collections.shuffle(this);
     }
 
+    /**
+     * prints out the cards contained in the deck in ascending index order
+     */
     private void printCards(){
         for (int i = 0; i <= 32; i++) {
             System.out.print(this.get(i) + " ");
         }
     }
-    //Test
 }
