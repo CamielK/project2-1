3fc2898c3cde19b6d34d4d0ffa9a2903eeabf290
Daniel Kaestner <DanielKaestner@outlook.de>
2017-09-14 12:41:58 +0200

    added shuffeling#

--- nothanks/src/Library/Deck.java
+++ nothanks/src/Library/Deck.java
@@ -1,12 +1,24 @@
 package Library;
 
 import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class Deck extends ArrayList<Card> {
-    
+
     public Deck() {
         for(int i = 3; i <= 35; i++) {
         	this.add(new Card(i));
+        }
+    }
+
+    private void shuffleCards(){
+        Collections.shuffle(this);
+    }
+
+    private void printCards(){
+        for (int i = 0; i <= 32; i++) {
+            System.out.print(this.get(i) + " ");
         }
     }
 }
