cf5d3d4e7e67b727481088f30663045bf6c08f59
Camiel <camielk@live.nl>
2017-11-15 19:56:33 +0100

    Initial commit MLR AI

---
+++ nothanks/src/Library/AI/MLR/MultipleLinearRegressionAI.java
@@ -0,0 +1,19 @@
+package Library.AI.MLR;
+
+import Library.AI.AIInterface;
+import Library.Player;
+
+import java.util.ArrayList;
+
+public class MultipleLinearRegressionAI implements AIInterface{
+
+    @Override
+    public boolean GetMove() {
+        return false;
+    }
+
+    @Override
+    public void gameIsFinished(ArrayList<Player> winner) {
+
+    }
+}
