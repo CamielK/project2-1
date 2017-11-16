724e62f06c8a7d596555126e81d23a2106798906
Camiel <camielk@live.nl>
2017-11-16 15:31:13 +0100

    Add never take AI

---
+++ nothanks/src/Library/AI/NevertakeAI/NevertakeAI.java
@@ -0,0 +1,20 @@
+package Library.AI.NevertakeAI;
+
+import Library.AI.AIInterface;
+import Library.Player;
+
+import java.util.ArrayList;
+
+public class NevertakeAI implements AIInterface {
+
+    @Override
+    public boolean GetMove() {
+        return false;
+    }
+
+    @Override
+    public void gameIsFinished(ArrayList<Player> winner) {
+        // TODO Auto-generated method stub
+    }
+
+}
